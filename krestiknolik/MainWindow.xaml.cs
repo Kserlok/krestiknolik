62e932a45385fd65ec19817bd0bf1308a339099d
Kserlok <73622335+Kserlok@users.noreply.github.com>
2021-06-09 19:56:10 +1000

    помогите

--- krestiknolik/MainWindow.xaml.cs
+++ krestiknolik/MainWindow.xaml.cs
@@ -13,16 +13,142 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace krestiknolik
+namespace krestiknolik 
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        string temp = "X";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        bool WinLose()
+        {
+            if (Button0.Content == Button1.Content && Button2.Content == Button1.Content)
+            {
+                return true;
+            }
+
+            else if (Button0.Content == Button3.Content && Button3.Content == Button6.Content)
+            {
+                return true;
+            }
+
+            else if (Button0.Content == Button4.Content && Button4.Content == Button8.Content)
+            {
+                return true;
+            }
+
+            else if (Button1.Content == Button4.Content && Button4.Content == Button7.Content)
+            {
+                return true;
+            }
+
+            else if (Button3.Content == Button4.Content && Button4.Content == Button5.Content)
+            {
+                return true;
+            }
+
+            else if (Button6.Content == Button4.Content && Button6.Content == Button2.Content)
+            {
+                return true;
+            }
+
+            else if (Button6.Content == Button7.Content && Button7.Content == Button8.Content)
+            {
+                return true;
+            }
+
+            else if (Button2.Content == Button5.Content && Button2.Content == Button8.Content)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        void Win()
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show("ВОТ ЭТО " + temp + " КЛАССНЫЕ ПАЦАНЫ", 
+                "WIIIN", MessageBoxButton.OKCancel,
+                MessageBoxImage.Error);
+            if (messageBoxResult == MessageBoxResult.OK)
+            {
+                try
+                {
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+       void Najat (Button button)
+       {
+            button.Content = temp;
+            button.IsEnabled = false;
+
+            if (WinLose())
+            {
+                Win();
+                return;
+            }
+
+            if (temp == "X") temp = "O";
+            else if (temp == "O") temp = "X";
+
+       }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Najat(Button0);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Najat(Button1);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Najat(Button2);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Najat(Button3);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Najat(Button4);
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Najat(Button5);
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            Najat(Button6);
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            Najat(Button7);
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            Najat(Button8);
         }
     }
 }
